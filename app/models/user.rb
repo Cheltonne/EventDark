151117c328a5227692c677a7db70b6c907378015
Cheltonne <chelton1506@gmail.com>
2020-02-18 15:18:31 +0100

    BU P7

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  after_create :welcome_send
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
+  
   has_many :participations
   has_many :events, through: :participations
   has_many :administrated_events, class_name: 'Event', foreign_key: 'admin_id'  
