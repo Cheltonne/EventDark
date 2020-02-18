16170e57a8cd4743bdec995fe39535fb6aef4c80
Cheltonne <chelton1506@gmail.com>
2020-02-18 23:32:34 +0100

    Haha les clés d'API :p

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   # Code is not reloaded between requests.
   config.cache_classes = true
-  config.action_mailer.default_url_options = { host: 'chelton1506@gmail.com' }
+  config.action_mailer.default_url_options = { host: 'https://nameless-chamber-80004.herokuapp.com/' }
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
