f5d134da460390396eb06b0fd62284fc1a9d65aa
Cheltonne <chelton1506@gmail.com>
2020-02-18 15:30:27 +0100

    BU P7

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -1,4 +1,10 @@
 class Participation < ApplicationRecord
+  after_create :welcome_send
+
+  def welcome_send
+    UserMailer.participation_email(self).deliver_now
+  end
+
   belongs_to :user
   belongs_to :event
 end
