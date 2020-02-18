3e1d42ea1086beff83275b11c387753588815611
Cheltonne <chelton1506@gmail.com>
2020-02-18 15:43:52 +0100

    BU P7

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -14,12 +14,12 @@ class UserMailer < ApplicationMailer
 
   def participation_email(user)
     #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
-    @user = user 
+    @event = user
 
     #on définit une variable @url qu'on utilisera dans la view d’e-mail
     @url  = 'http://monsite.fr/login' 
 
     # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
-    mail(to: @user.email, subject: 'Salut les mouss\', alors comme ca ca mouss\' ???????') 
+    mail(to: @event.user.email, subject: 'Salut les mouss\', alors comme ca ca mouss\' ???????') 
   end
 end
